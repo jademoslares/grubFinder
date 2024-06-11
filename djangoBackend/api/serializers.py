06ec1cdd35150a054cbfa285b0f05e4c43814208
jademoslares <jade.moslares@gmail.com>
2024-06-10 23:30:16 -0400

    setup sample urls

--- djangoBackend/api/serializers.py
+++ djangoBackend/api/serializers.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from rest_framework import serializers
 from .models import Note
 
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
--- djangoBackend/api/views.py
+++ djangoBackend/api/views.py
@@ -5,6 +5,7 @@ from .serializers import UserSerializer,NoteSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from .models import Note
 
+
 class NoteListCreate(generics.ListCreateAPIView):
     serializer_class = NoteSerializer
     permission_classes = [IsAuthenticated]
@@ -19,6 +20,15 @@ class NoteListCreate(generics.ListCreateAPIView):
         else:
             print(serializer.errors)
         return
+    
+
+class NoteDelete(generics.DestroyAPIView):
+    serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Note.objects.filter(author=user)
 
 class CreateUserView(generics.ListCreateAPIView):
     queryset = User.objects.all()
