5dbb0f6f5f85245d2e92e7428787d6c07173c9b4
jademoslares <jade.moslares@gmail.com>
2024-06-17 16:11:53 -0400

    new pages

--- reactFrontend/src/App.jsx
+++ reactFrontend/src/App.jsx
@@ -1,16 +1,34 @@
 import react from 'react'
-import { BrowserRouter,Route,Navigateion } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 import LoginPage from './pages/LoginPage/LoginPage'
 import HomePage from './pages/HomePage/HomePage'
 import NotFound from './pages/NotFound'
 import ProtectedRoute from './components/ProtectedRoute'
 
 
+function Logout(){
+  localStorage.clear()
+  return <Navigate to="/login" />
+}
+
+function RegisterAndLogout(){
+  localStorage.clear()
+  return <Navigate to="/login" />
+
+}
+
 function App() {
 
   return (
     <>
-
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ProtectedRoute> <HomePage /> </ProtectedRoute>} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterAndLogout />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      </BrowserRouter>
     </>
   )
 }
