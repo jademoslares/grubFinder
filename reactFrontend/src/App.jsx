c59734fbb892873ee12fad065ba9b2d74548002b
jademoslares <jade.moslares@gmail.com>
2024-06-17 15:41:01 -0400

    new Pages

--- reactFrontend/src/App.jsx
+++ reactFrontend/src/App.jsx
@@ -1,4 +1,10 @@
 import react from 'react'
+import { BrowserRouter,Route,Navigateion } from 'react-router-dom'
+import LoginPage from './pages/LoginPage/LoginPage'
+import HomePage from './pages/HomePage/HomePage'
+import NotFound from './pages/NotFound'
+import ProtectedRoute from './components/ProtectedRoute'
+
 
 function App() {
 
