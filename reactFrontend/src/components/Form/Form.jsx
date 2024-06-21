9a60c785e9894c825667e3ac6187331863921adb
jademoslares <jade.moslares@gmail.com>
2024-06-20 21:26:39 -0400

    form setup

--- reactFrontend/src/components/Form/Form.jsx
+++ reactFrontend/src/components/Form/Form.jsx
@@ -2,3 +2,10 @@ import { useState } from "react";
 import {api} from "../../api";
 import { useNavigate } from "react-router-dom";
 import { ACCESS_TOKEN,REFRESH_TOKEN } from "../../constants";
+
+function Form({route,method}){
+    const [username,setUsername] = useState("")
+    const [password,setPassword] = useState("")
+    const [loading,setLoading] = useState(false)
+    const navigate = useNavigate()
+}
